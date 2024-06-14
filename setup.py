c83e5cb8f9ffc3bbdeeab620cbf10e33945f2891
Thalles Lima <137906445+thalleslimasys@users.noreply.github.com>
2024-06-14 13:28:54 +0000

    ajouté docstring for setup

--- setup.py
+++ setup.py
@@ -1,3 +1,15 @@
+"""
+Ce fichier de configuration est utilisé pour configurer la distribution
+du package Python. Il contient des informations essentielles telles que le nom
+du package, la version, les packages inclus et les dépendances requises.
+
+Ce fichier utilise setuptools, une bibliothèque pour faciliter la création,
+la distribution et l'installation de packages Python.
+
+Pour plus d'informations sur l'utilisation de setuptools, consultez :
+https://setuptools.pypa.io/en/latest/userguide/quickstart.html
+"""
+
 from setuptools import setup, find_packages
 
 setup(
